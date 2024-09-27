4b4cf55968625a50e45f7b5381dde17d6fa2ad05
Beatriz Azevedo <bia.azevedo@live.com.pt>
2024-09-27 14:47:41 +0100

    Added footer

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import "./styles.css";
-import Weather from "./Weather.js";
+import './styles.css';
+import Weather from './Weather.js';
 
 export default function App() {
   return (
@@ -7,6 +7,14 @@ export default function App() {
       <h1>Weather APP</h1>
       <h2>Check the weather in your city</h2>
       <Weather />
+      <footer>
+        This project was coded by{' '}
+        <a href="https://github.com/beatrizevedo">Beatriz Azevedo</a>,
+        open-sourced on{' '}
+        <a href="https://github.com/beatrizevedo/weather-react">Github</a>,
+        hosted on{' '}
+        <a href="https://beatriz-weather-react.netlify.app/">Netlify</a>
+      </footer>
     </div>
   );
 }
